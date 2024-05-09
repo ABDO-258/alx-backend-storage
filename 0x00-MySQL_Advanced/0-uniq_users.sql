e8356c348857611f560572ad0564bf10bbe8cbe1
karbal abdelhadi <abdoraja2005@gmail.com>
2024-05-09 18:17:40 +0100

    task0fix comment problem

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- script that creates a table users following these requirements:
 -- With these attributes:
---id, integer, never null, auto increment and primary key
---email, string (255 characters), never null and unique
---name, string (255 characters)
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
 
 CREATE TABLE usersIF NOT EXISTS `users` (
     `id` INT AUTO_INCREMENT PRIMARY KEY ,
