08700e86d6f3925e3df999b9261c53631c6abd51
karbal abdelhadi <abdoraja2005@gmail.com>
2024-05-13 20:19:37 +0100

    task12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""returns the list of school having a specific topic"""
+
+
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
+insert_school = __import__('9-insert_school').insert_school
+schools_by_topic = __import__('11-schools_by_topic').schools_by_topic
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_db = client.logs.nginx
+
+    print("{} logs".format(logs_db.estimated_document_count()))
+    print("Methods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = logs_db.count_documents({'method': method})
+        print("\tmethod {}: {}".format(method, count))
+
+    documents_get = logs_db.count_documents(
+        {'method': 'GET', 'path': "/status"})
+    print("{} status check".format(documents_get))
