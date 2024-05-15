1226019437ab4045d412737533e0eb1b4331f1e5
karbal abdelhadi <abdoraja2005@gmail.com>
2024-05-15 16:27:56 +0100

    task[1]

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """Module for redis task"""
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
 import redis
-
-
 
 
 class Cache:
@@ -20,3 +18,25 @@ class Cache:
         # Store data in Redis with the generated key
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """get data with key """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieves data from Redis, converts it to an integer, and returns it.
+        """
+        data = self._redis.get(key).decode("utf-8")
+        return data
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieves data from Redis, converts it to an integer, and returns it.
+        """
+        data = int(self._redis.get(key).decode("utf-8"))
+        return data
